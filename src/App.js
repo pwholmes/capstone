adb33025ebd8f1d13c16ca1a39206dfb4338ba8a
pwholmes <pwholmes151@gmail.com>
2024-08-21 10:46:49 -0400

    removed Chakra UI

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import { ChakraProvider } from '@chakra-ui/react'
 import '@fontsource-variable/karla';
 import '@fontsource-variable/markazi-text';
 
 function App() {
   return (
     <>
-    <ChakraProvider>
       <Nav/>
       <Main />
       <Footer />
-    </ChakraProvider>
     </>
   );
 }
