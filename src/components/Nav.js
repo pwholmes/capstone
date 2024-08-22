8bd5a48823f70d74dac63ac06ca6bf4af64aedd2
pwholmes <pwholmes151@gmail.com>
2024-08-22 15:26:44 -0400

    added unit tests for availableTimes reducer functions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ import BookingPage from '../components/BookingPage';
 
 const defaultAvailableTimes = ["11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00"];
 
-const updateTimes = (availableTimes, date) => {
+const updateTimes = (prevAvailableTimes, date) => {
     // To test the reducer functionality: if the user selects the first day of the month in the date picker on the Bookings Form,
     // return only 12 noon as the available time.  Otherwise return the default times.
     let x = new Date(date).getUTCDate();
@@ -43,3 +43,4 @@ function Nav() {
 }
 
 export default Nav;
+export { initializeTimes, updateTimes, defaultAvailableTimes };
---
+++ src/components/Nav.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from './Nav';
+import { initializeTimes, updateTimes, defaultAvailableTimes } from './Nav';
+
+test("Test initializeTimes reducer function", () => {
+    const availableTimes = initializeTimes();
+    expect(JSON.stringify(availableTimes) == JSON.stringify(defaultAvailableTimes));
+})
+
+test("Test updateTimes reducer function", () => {
+    const prevAvailableTimes = [];
+    const date = "2024-08-22";
+    const availableTimes = updateTimes(prevAvailableTimes, date);
+    expect(JSON.stringify(availableTimes) == JSON.stringify(defaultAvailableTimes));
+})
