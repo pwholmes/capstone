48979530fd5510b74b2a78ed13bed56363b66436
pwholmes <pwholmes151@gmail.com>
2024-08-23 18:59:28 -0400

    massively beefed up form validation; added positive unit test; fixed unit test bug concerning available times

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,16 +1,44 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// Set the date ranges allowed for reservations.  We allow then up to 2 months in advance
-const now = new Date();
-const nowDate = now.toISOString().substring(0, 10);
-now.setMonth(now.getMonth() + 2);
-const laterDate = now.toISOString().substring(0, 10);
 
 function BookingForm(props) {
-    const defaultFormData = {date: "", time: "", numGuests: 1, event: "none", name: "", email: "", ccNum: ""};
+    const defaultFormData = {date: "", dateTouched: false, 
+                             time: "", timeTouched: false,
+                             numGuests: 1, numGuestsTouched: false,
+                             event: "none", 
+                             name: "", nameTouched: false,
+                             email: "", emailTouched: false};
     const [formData, setFormData] = useState(defaultFormData);
     const navigate = useNavigate();
+
+    // Set the date ranges allowed for reservations: between today, and two months from today.
+    // I know this code is janky, but it's the only way I could make the dates compare correctly.
+    // Even using setHours(0,0,0,0) didn't work because of the time zone adjustments, and rather than
+    // deal with the nightmare of time zones, I just stringified the dates, chopped off the time portions,
+    // and re-converted them into dates.
+    const rawNow = new Date();
+    const nowString = rawNow.toISOString().substring(0, 10);
+    const now = new Date(nowString);
+
+    const rawLater = new Date();
+    rawLater.setMonth(rawLater.getMonth() + 2);
+    const laterString = rawLater.toISOString().substring(0, 10);
+    const later = new Date(laterString);
+
+    // Validate each field
+    let dateIsValid = false;
+    if (formData.date !== "") {
+        const rawFormDate = new Date(formData.date);
+        const rawFormDateString = rawFormDate.toISOString().substring(0, 10);
+        const formDate = new Date(rawFormDateString);
+        dateIsValid = formDate.getTime() >= now.getTime() && formDate.getTime() <= later.getTime();
+    }
+    const timeIsValid = (formData.time !== "");
+    const numGuestsIsValid = (formData.numGuests >= 1 && formData.numGuests <= 10);
+    const nameIsValid = (formData.name !== "");
+    const emailIsValid = (formData.email === "" || formData.email.includes("@"));
+    const isConfirmDisabled = !dateIsValid || !timeIsValid || !numGuestsIsValid || !nameIsValid || !emailIsValid;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -27,22 +55,28 @@ function BookingForm(props) {
                     <h2 className="bookSubheading">Reservation Info</h2>
 
                     <label className="bookText bookLabel" htmlFor="bookDateInput">Date*</label>
-                    <input className="bookText bookInput" id="bookDateInput" type="date" value={formData.date} min={nowDate} max={laterDate}
+                    <input className="bookText bookInput" id="bookDateInput" type="date" value={formData.date} min={nowString} max={laterString}
+                        onBlur={(e) => setFormData(prevState => ({...prevState, dateTouched: true})) }
                         onChange={(e) => {
                             props.dispatcher(e.target.value);
                             return setFormData(prevState => ({...prevState, date: e.target.value, time: ""}))
                         }}/><br/>
+                    { (formData.dateTouched && !dateIsValid) ? <p className="bookInputError">The Date must be between today and two months from today.</p> : ""}
 
                     <label className="bookText bookLabel" htmlFor="bookTimeInput">Time*</label>
                     <select className="bookText bookInput" id="bookTimeInput" value={formData.time}
-                            onChange={(e) => setFormData(prevState => ({...prevState, time: e.target.value}))}>
+                        onBlur={(e) => setFormData(prevState => ({...prevState, timeTouched: true})) }
+                        onChange={(e) => setFormData(prevState => ({...prevState, time: e.target.value}))}>
                         <option value="">--select--</option>
                         {props.availableTimes.map((time,i) => <option key={i} value={time}>{time}</option>)}
                     </select><br/>
+                    { (formData.timeTouched && !timeIsValid) ? <p className="bookInputError">Time is required.</p> : ""}
 
                     <label className="bookText bookLabel" htmlFor="bookNumGuestsInput">Number of guests*</label>
                     <input className="bookText bookInput" id="bookNumGuestsInput" type="number" placeholder="1" min="1" max="10" value={formData.numGuests}
+                        onBlur={(e) => setFormData(prevState => ({...prevState, numGuestsTouched: true})) }
                         onChange={(e) => setFormData(prevState => ({...prevState, numGuests: e.target.value}))} /><br/>
+                    { (formData.numGuestsTouched && !numGuestsIsValid) ? <p className="bookInputError">The number of guests must be between 1 and 10.</p> : ""}
 
                     <label className="bookText bookLabel" htmlFor="bookEventInput">Type of event</label>
                     <select className="bookText bookInput" id="bookEventInput" value={formData.event}
@@ -54,7 +88,7 @@ function BookingForm(props) {
                         <option value="reuniom">Reunion</option>
                         <option value="other">Other</option>
                     </select>
-                    <p>* indicates required field</p>
+                    <p className="bookRequired">* indicates required field</p>
                 </section>
 
                 <section className="bookInputGroup">
@@ -62,11 +96,15 @@ function BookingForm(props) {
 
                     <label className="bookText bookLabel" htmlFor="bookName">Name*</label>
                     <input className="bookText bookInput" id="bookName" type="text"  placeholder="Name" value={formData.name}
+                        onBlur={(e) => setFormData(prevState => ({...prevState, nameTouched: true})) }
                         onChange={(e) => setFormData(prevState => ({...prevState, name: e.target.value}))} /><br/>
+                    { (formData.nameTouched && !nameIsValid) ? <p className="bookInputError">A Name is required.</p> : ""}
 
                     <label className="bookText bookLabel" htmlFor="bookEmail">Email Address</label>
                     <input className="bookText bookInput" id="bookEmail" type="email" placeholder="Email Address" value={formData.email}
+                        onBlur={(e) => setFormData(prevState => ({...prevState, emailTouched: true})) }
                         onChange={(e) => setFormData(prevState => ({...prevState, email: e.target.value}))} /><br/>
+                    { (formData.emailTouched && !emailIsValid) ? <p className="bookInputError">Email Address is optional, but if provided it must contain a @.</p> : ""}
 
                     {/*
                     <label className="bookText bookLabel" htmlFor="bookCreditCardNum">Credit Card Number*</label>
@@ -74,13 +112,13 @@ function BookingForm(props) {
                         onChange={(e) => setFormData(prevState => ({...prevState, ccNum: e.target.value}))} /><br/>
                     */}
 
-                    <p>If you provide an email address, we will send you a reminder the day before your reservation.  The email address is not used for any other purpose.</p><br/>
-                    <p>* indicates required field</p>
+                    <p className="bookEmail">If you provide an email address, we will send you a reminder the day before your reservation.  The email address is not used for any other purpose.</p>
+                    <p className="bookRequired">* indicates required field</p>
                     </section>
 
                 <section className="bookButtonContainer">
                     <button className="button bookConfirmButton" type="submit"
-                        disabled={(!formData.date || !formData.time || !formData.name)}>Confirm</button>
+                        disabled={isConfirmDisabled}>Confirm</button>
                 </section>
             </form>
         </section>
--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -20,15 +20,11 @@ test("Check if Booking Form loads", () => {
 // - the date is out of range (before the current date, or more than 2 months in the future)
 // - the number of guests is <1 or >10
 // - the email address is invalid (it doesn't include a @)
-//
-// The browser already detects all three possibilities via the rules provided by the appropriate
-// HTML attributes, and posts appropriate error messages when the user clicks the Confirm button, preventing
-// the form from being submitted until these errors are corrected.
-// So what we have to test is that if the fields are provided with invalid data, the submitForm() API
-// is NOT getting called, because the HTML validation rules block that from happening.
+// We have to ensure that if the fields are provided with invalid data, the Confirm button is
+// disabled and the submitForm() API is NOT getting called if they try to click it.
 
 test("Check if invalid dates are detected", () => {
-    const availableTimes = ["12:00"];
+    const availableTimes = ["17:00"];
     const dispatcher = jest.fn();
     const submitForm = jest.fn();
     render(
@@ -63,7 +59,7 @@ test("Check if invalid dates are detected", () => {
 
 
 test("Check if invalid number of guests is detected", () => {
-    const availableTimes = ["12:00"];
+    const availableTimes = ["17:00"];
     const dispatcher = jest.fn();
     const submitForm = jest.fn();
     render(
@@ -95,8 +91,9 @@ test("Check if invalid number of guests is detected", () => {
     expect(submitForm).not.toHaveBeenCalled();
 })
 
+
 test("Check if invalid email address is detected", () => {
-    const availableTimes = ["12:00"];
+    const availableTimes = ["17:00"];
     const dispatcher = jest.fn();
     const submitForm = jest.fn();
     render(
@@ -130,4 +127,42 @@ test("Check if invalid email address is detected", () => {
     const confirmButton = screen.getByRole("button");
     fireEvent.click(confirmButton);
     expect(submitForm).not.toHaveBeenCalled();
+})
+
+
+test("Check if form can be submitted if all fields are valid", () => {
+    const availableTimes = ["17:00"];
+    const dispatcher = jest.fn();
+    const submitForm = jest.fn();
+    render(
+        <BrowserRouter>
+            <BookingForm availableTimes={availableTimes} dispatcher={dispatcher} submitForm={submitForm} />
+        </BrowserRouter>
+    );
+
+    // Fill fields
+    // This time the email field gets in invalid value.
+    // It's not a required field but it still has to have a valiad value if we do fill it out!
+    // Fill other required fields with valid data
+    const date = new Date();
+    const dateStr = date.toISOString().substring(0, 10);
+    const dateInput = screen.getByLabelText(/Date/);
+    fireEvent.change(dateInput, { target: { value: dateStr }});
+
+    const timeInput = screen.getByLabelText(/Time/);
+    fireEvent.change(timeInput, { target: { value: "17:00" }});
+
+    const guestsInput = screen.getByLabelText(/guests/);
+    fireEvent.change(guestsInput, { target: { value: "2" }});
+
+    const nameInput = screen.getByLabelText(/Name/);
+    fireEvent.change(nameInput, { target: { value: "Bob" }});
+
+    const emailInput = screen.getByLabelText(/Email/);
+    fireEvent.change(emailInput, { target: { value: "xxx@gmail.com" }});
+
+    // Click the Confirm button and verify that the submitForm function is called
+    const confirmButton = screen.getByRole("button");
+    fireEvent.click(confirmButton);
+    expect(submitForm).toHaveBeenCalled();
 })
